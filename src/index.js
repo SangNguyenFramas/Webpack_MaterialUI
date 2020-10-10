cbd07538aa0bdc93780272500778f1d7a59bd1eb
NTS333 <42103635+NTS333@users.noreply.github.com>
2020-10-10 13:46:34 +0700

    Webpack
    Setup webpack

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import './index.scss';
+
+const App = () => {
+  return (
+    <div class="wrapper">
+        Hello
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
